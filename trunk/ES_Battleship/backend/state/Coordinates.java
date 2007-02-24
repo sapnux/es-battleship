f69675824baea0ba3d17d8162c1c9bf0b842d385
navid.azimi <navid.azimi@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-02-24 23:36:44 +0000

    forgot to add this to the repository

---
+++ trunk/ES_Battleship/backend/state/Coordinates.java
@@ -0,0 +1,39 @@
+package backend.state;
+
+public class Coordinates {
+	
+	private int x;
+	private int y;
+	
+	/**
+	 * Get the X coordinate.
+	 * @return
+	 */
+	public int getX() {
+		return x;
+	}
+	
+	/**
+	 * Set the X coordinate.
+	 * @param x
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	/**
+	 * Get the Y coordinate.
+	 * @return
+	 */
+	public int getY() {
+		return y;
+	}
+	
+	/**
+	 * Set the Y coordinate.
+	 * @param y
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+}
