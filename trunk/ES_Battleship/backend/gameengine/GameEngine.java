d34d2508e371308dd13421339ab35a598be9a0b8
genek81 <genek81@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-02-24 16:24:14 +0000

    more client/server work

---
+++ trunk/ES_Battleship/backend/gameengine/GameEngine.java
@@ -0,0 +1,5 @@
+package backend.gameengine;
+
+public class GameEngine {
+
+}
--- trunk/ES_Battleship/backend/state/Player.java
+++ trunk/ES_Battleship/backend/state/Player.java
@@ -3,10 +3,24 @@ package backend.state;
 public class Player {
 	
 	private String id;
-	private Board board;
+	private Board myBoard;
+	private Board oppBoard;
 	
 	public Player(String id, Board board) {
 		this.id = id;
-		this.board = board;
+		this.myBoard = board;
+		this.oppBoard = new Board();
+	}
+
+	public Board getMyBoard() {
+		return myBoard;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public Board getOppBoard() {
+		return oppBoard;
 	}
 }
