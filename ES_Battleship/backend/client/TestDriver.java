54e949f8855a6fc08e62dd704a75838b86edf87a
navid.azimi <navid.azimi@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-02-24 06:14:22 +0000

    - fixed board print out bug - finished the board add() method 	- added fourth boolean parameter (needs to update with Orientation enum) - added some sample code in the TestDriver - updated the board print() method to number rows and columns for easier debugging

--- ES_Battleship/backend/client/TestDriver.java
+++ ES_Battleship/backend/client/TestDriver.java
@@ -1,19 +1,29 @@
 package backend.client;
 
 import backend.state.Board;
+import backend.state.ships.*;
 
 public class TestDriver {
 	
 	public static void main (String args[]) throws InterruptedException {
-		Client cl = new Client(new Board());
-		String server = args[0];
-		String port = args[1];
-		cl.connect(server, port);
-		cl.sendTestPacket();
-		Thread.sleep(5000);
-		cl.move(2, 3);
-		Thread.sleep(5000);
-		cl.disconnect();
+		
+		Board board = new Board();
+		board.add(Ships.GetAircraftCarrier(), 0, 0, true);
+		board.add(Ships.GetBattleship(), 1, 1, false);
+		board.add(Ships.GetCruiser(), 6, 4, true);
+		board.add(Ships.GetPatrolBoat(), 6, 9, false);
+		board.add(Ships.GetSubmarine(), 5, 5, true);
+		board.print();
+		
+//		Client cl = new Client(board);
+//		String server = args[0];
+//		String port = args[1];
+//		cl.connect(server, port);
+//		cl.sendTestPacket();
+//		Thread.sleep(5000);
+//		cl.move(2, 3);
+//		Thread.sleep(5000);
+//		cl.disconnect();
 	}
 	
 }
