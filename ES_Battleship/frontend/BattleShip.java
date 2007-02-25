fa28ba347a9664ecfb5b36175c9ba8f390ee517e
fasmith718 <fasmith718@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-02-25 20:11:01 +0000

    Implemented connections between backend objects and frontend displays.  Only a small amount of code yet needs to be added to the OpponentPanel to get it to properly draw hits & misses on the screen.

--- ES_Battleship/frontend/BattleShip.java
+++ ES_Battleship/frontend/BattleShip.java
@@ -1,14 +1,36 @@
 package frontend;
 
+import backend.client.*;
+import backend.state.*;
+import frontend.test.*;
+
 public class BattleShip {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		EsbBattleWindow foo = new EsbBattleWindow();
-		foo.setVisible(true);
+		
+		if (args.length != 2) {
+			System.err.println("Usage: BattleShip <server-ip-address> <server-port>");
+			System.exit(-1);
+		}
+
+		//Fleet arrangement section
+		//--------------------------
+		
+		//TEST CODE
+		String playerID = "player1";
+		Board theBoard = new Board();
+		TestClient theClient = new TestClient(playerID, theBoard);
+		
+		//Gameplay Section
+		EsbFrontendController theFController = 
+				new EsbFrontendController(theClient);
+		
+		EsbBattleWindow theBWindow = new EsbBattleWindow(theFController);
+		theBWindow.setVisible(true);
+		//--------------------------
 	}
 
 }
