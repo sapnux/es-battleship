1101886fb2ac86e99efac9dc80528c140544d327
genek81 <genek81@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-02-28 04:48:56 +0000

    refactoring

---
+++ trunk/ES_Battleship/backend/test/state/ships/ShipsTest.java
@@ -0,0 +1,33 @@
+package backend.state.ships.test;
+
+import junit.framework.TestCase;
+import backend.state.ships.*;
+
+public class ShipsTest extends TestCase {
+
+	public void testGetAircraftCarrier() {
+		IShip ship =  Ships.GetAircraftCarrier();
+		assertEquals("Wrong ship class instance", AircraftCarrier.class, ship.getClass());
+	}
+
+	public void testGetBattleship() {
+		IShip ship =  Ships.GetBattleship();
+		assertEquals("Wrong ship class instance", Battleship.class, ship.getClass());
+	}
+
+	public void testGetCruiser() {
+		IShip ship =  Ships.GetCruiser();
+		assertEquals("Wrong ship class instance", Cruiser.class, ship.getClass());
+	}
+
+	public void testGetPatrolBoat() {
+		IShip ship =  Ships.GetPatrolBoat();
+		assertEquals("Wrong ship class instance", PatrolBoat.class, ship.getClass());
+	}
+
+	public void testGetSubmarine() {
+		IShip ship =  Ships.GetSubmarine();
+		assertEquals("Wrong ship class instance", Submarine.class, ship.getClass());
+	}
+
+}
