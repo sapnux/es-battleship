f0862213572716e5960d799f2d55da50d751d84a
genek81 <genek81@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-03-03 18:18:49 +0000

    added a couple of missing tests

---
+++ trunk/ES_Battleship/backend/test/state/CoordinatesTest.java
@@ -0,0 +1,43 @@
+package backend.test.state;
+
+import junit.framework.TestCase;
+
+public class CoordinatesTest extends TestCase {
+
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	public void testCoordinates() {
+		fail("Not yet implemented");
+	}
+
+	public void testCoordinatesIntInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetX() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetY() {
+		fail("Not yet implemented");
+	}
+
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ trunk/ES_Battleship/backend/test/state/PlayerTest.java
@@ -0,0 +1,59 @@
+package backend.test.state;
+
+import junit.framework.TestCase;
+
+public class PlayerTest extends TestCase {
+
+	public void testSetChanged() {
+		fail("Not yet implemented");
+	}
+
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMyBoard() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetOppBoard() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetGameResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetGameResult() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsMyTurn() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetMyTurn() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMessages() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddMessage() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrintMessages() {
+		fail("Not yet implemented");
+	}
+
+	public void testResetMessages() {
+		fail("Not yet implemented");
+	}
+
+}
