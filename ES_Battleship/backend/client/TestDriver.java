d52c8cb4f149529ee99df7b7b6ab0c1f2c61a87e
navid.azimi <navid.azimi@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-04-07 03:49:55 +0000

    backend code complete

--- ES_Battleship/backend/client/TestDriver.java
+++ ES_Battleship/backend/client/TestDriver.java
@@ -3,7 +3,14 @@ package backend.client;
 public class TestDriver {
 	
 	public static void main (String args[]) throws Exception {
-		//new SampleClient(args[0], args[1], args[2]);
-		new SampleEJBClient(args[0], args[1]);
+		if (args == null) {
+			return;
+		}
+
+		if (args.length == 3) {
+			new SampleClient(args[0], args[1], args[2]);
+		} else if (args.length == 2) {
+			new SampleEJBClient(args[0], args[1]);
+		}
 	}	
 }
