71b7bdf6660cd298466f246cbb4de1ffbd056d40
navid.azimi <navid.azimi@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-04-07 19:38:51 +0000

    - IEJBClient now extends IClient - the connect() and disconnect() methods have been added to EJBClient but DO NOTHING
    This was done in order to be more compatible with the frontend ui.

--- ES_Battleship/backend/client/IClient.java
+++ ES_Battleship/backend/client/IClient.java
@@ -5,7 +5,7 @@ import backend.state.Player;
 public interface IClient {
 	void connect(String server, String port);
 	void disconnect();
-	boolean move(int x, int y);
+	void move(int x, int y);
 	Player getPlayer();
 	void signalReadiness();
 	void waitForTurn();
--- ES_Battleship/backend/client/IEJBClient.java
+++ ES_Battleship/backend/client/IEJBClient.java
@@ -2,7 +2,7 @@ package backend.client;
 
 import backend.state.Player;
 
-public interface IEJBClient {
+public interface IEJBClient extends IClient {
 	void move(int x, int y);
 	Player getPlayer();
 	void signalReadiness();
