f36d572f1c75e714fdf474446f6f0cdbaad83ee4
fasmith718 <fasmith718@8e5966ce-7428-0410-a350-6b9ca30f0593>
2007-04-26 00:59:39 +0000

    - fixed validateCoordinates in Board.java - added new validations to Deserialize in Board.java - improved BoardTest: testNullCheck(), testEquals(), testDeserialize(), testSetCoordinates(), etc. - removed some commented text from EsbArrangmentWindow.

--- ES_Battleship/backend/test/state/BoardTest.java
+++ ES_Battleship/backend/test/state/BoardTest.java
@@ -1,6 +1,8 @@
 package backend.test.state;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 import java.util.Random;
 
 import junit.framework.TestCase;
@@ -8,8 +10,8 @@ import backend.constants.Constants;
 import backend.constants.Orientation;
 import backend.state.Board;
 import backend.state.Coordinates;
-import backend.state.ships.Ships;
 import backend.util.BackendException;
+import backend.state.ships.*;
 
 public class BoardTest extends TestCase {
 
@@ -84,18 +86,31 @@ public class BoardTest extends TestCase {
 		}
 		assertEquals("Losing condition not determined correctly", true, tmpBoard.hasLost());
 	}
-
+	
+	/**
+	 * Contains the tests for setCoordinate as well as the tests for
+	 * validateCoordinates, which is private and called by 
+	 * setCoordinates.
+	 */
 	public void testSetCoordinate() {
-		for (int y = 0; y < this.board.length(); y++) {
-			for (int x = 0; x < this.board.length(); x++) {
+		//Test border cases by starting @ -1 and finishing @ length
+		//There should be 44 exceptions thrown
+		int tNumExceptions = 0;
+		for (int y = -1; y <= this.board.length(); y++) {
+			for (int x = -1; x <= this.board.length(); x++) {
 				try {
 					this.board.setCoordinate('!', x, y);
 					assertEquals('!', this.board.getCoordinate(x, y));
 				} catch (BackendException e) {
-					e.printStackTrace();
-				}
+					//e.printStackTrace();
+					tNumExceptions++;
+					assertEquals(e.getMessage(), 
+							"The coordinates (" + x + ", " + y + ") is out of bounds.");
+					assertTrue((x<0)||(y<0)||(x>=this.board.length())||(y>=this.board.length()));
+				}			
 			}
 		}
+		assertEquals(tNumExceptions, 44);
 	}
 
 	public void testGetCoordinate() {
@@ -120,10 +135,90 @@ public class BoardTest extends TestCase {
 				int y = coordinates.getY();
 				assertEquals("Ship not at coordinates", Ships.getAircraftCarrier().getSymbol(), 
 						this.board.getCoordinate(x, y));
-			}
+			}			
 		} catch (BackendException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void testNullCheck(){
+		try {
+			board.getShipCoordinates(null);
+		} catch (BackendException e) {
+			assertEquals(e.getMessage(),
+					"Some object instance is null.");
+		}
+	}
+	
+	public void testValidate(){
+		List<IShip> tShips = Ships.getAllShips();
+		Iterator tShipIter = tShips.iterator();
+		IShip tCurShip;
+		
+		//test boundaries
+		while (tShipIter.hasNext()) {			
+			tCurShip = (IShip)tShipIter.next();
+			int tStartCell = (this.board.length()+1) - tCurShip.getSize();
+			for(int i=tStartCell;i<this.board.length(); i++){
+				for(int j = 0; j<this.board.length(); j++){
+					try {
+						board.add(tCurShip, i, j, Orientation.HORIZONTAL);
+						fail("Exception not thrown, horizontal, "+ 
+								tCurShip.getName()+", i: "+
+								i + ", j: "+j);
+					} catch (BackendException e) {
+						assertEquals(e.getMessage(),
+								"The placement of the " + tCurShip.getName()
+								+ " is out of bounds.");
+					}
+					try {
+						board.add(tCurShip, j, i, Orientation.VERTICAL);
+						fail("Exception not thrown, vertical, "+ 
+								tCurShip.getName()+", i: "+
+								i + ", j: "+j);
+					} catch (BackendException e) {
+						assertEquals(e.getMessage(),
+								"The placement of the " + tCurShip.getName()
+								+ " is out of bounds.");
+					}
+				}//for j
+			}// for i
+		}// while
+		
+		//test overlap
+		try {
+			board.add(Ships.getAircraftCarrier(), 3, 4, Orientation.HORIZONTAL);
+		} catch (BackendException e) {
+			e.printStackTrace();
+		}
+		tShipIter = tShips.iterator();
+		while(tShipIter.hasNext()){
+			tCurShip = (IShip)tShipIter.next();
+			
+			//cross Vertically
+			try {
+				board.add(tCurShip, 4, 4, Orientation.VERTICAL);
+				fail("Exception not thrown, vertical, "+ 
+						tCurShip.getName());
+			} catch (BackendException e) {
+				assertEquals(e.getMessage(),
+						"The coordinate (" + 4 + "," + 4
+						+ ") is already occupied. Cannot place "
+						+ tCurShip.getName() + ".");
+			}
+			
+			//cross Horizontally
+			try {
+				board.add(tCurShip, 4, 4, Orientation.HORIZONTAL);
+				fail("Exception not thrown, horizontal, "+ 
+						tCurShip.getName());
+			} catch (BackendException e) {
+				assertEquals(e.getMessage(),
+						"The coordinate (" + 4 + "," + 4
+						+ ") is already occupied. Cannot place "
+						+ tCurShip.getName() + ".");
+			}
+		}//while
 	}
 
 	public void testGetAllMissedCoordinates() {
@@ -241,6 +336,11 @@ public class BoardTest extends TestCase {
 		testBoard.add(Ships.getAircraftCarrier(), 0, 0, Orientation.VERTICAL);
 		testBoard.add(Ships.getPatrolBoat(), 6, 9, Orientation.HORIZONTAL);
 		assertEquals("equals() implementation incorrect", testBoard, board);
+		
+		assertFalse(testBoard.equals(null));
+		assertFalse(testBoard.equals(new Coordinates(1,0)));
+		
+		assertFalse(testBoard.equals(new Board()));		
 	}
 	
 	public void testToString() throws BackendException {
@@ -249,10 +349,49 @@ public class BoardTest extends TestCase {
 		assertEquals("Serialization failed", expected, actual);
 	}
 
-	public void testDeserialize() throws BackendException {
+	public void testDeserialize(){
 		String tmpString = "a*********a*********a*********a*********a*******************************************************pp**";
-		Board tmpBoard = Board.deserialize(tmpString);
-		assertEquals("Deserialization failed", this.board, tmpBoard);
+		Board tmpBoard;
+		try {
+			tmpBoard = Board.deserialize(tmpString);
+			assertEquals("Deserialization failed", this.board, tmpBoard);			
+		} catch (BackendException e1) {
+			e1.printStackTrace();
+		}
+
+		//test null value
+		try {
+			Board.deserialize(null);
+			fail("no exception thrown for null");
+		} catch (BackendException e) {
+			assertEquals(e.getMessage(),
+				"The String paramater was null.");
+		}
+		
+		//test invalid length string
+		String tTestStr = "";
+		for(int i=0; i<Math.pow(this.board.length(), 2)+1; i++){
+			try {
+				Board.deserialize(tTestStr);
+				if(i!=100)
+					fail("string of length " + i + "and no exception");
+			} catch (BackendException e) {
+				assertEquals(e.getMessage(),
+						"The String paramater was the wrong length.");
+			}
+			
+			tTestStr+="a";
+		}
+		
+		//test a string with bad characters in it
+		tmpString = tmpString.replace('a', 'F');
+		try {
+			Board.deserialize(tmpString);
+			fail("Did not pickup on bad character.");
+		} catch (BackendException e) {
+			assertEquals(e.getMessage(),
+					"Character F is an invalid character.");
+		}
 	}
 
 	public void testReset() throws BackendException {
